fb02b46b42257bda3dc45a48e92515f124ed2843
Antoine Salliou <asalliou@tp.michelin.com>
2020-12-29 06:58:59 +0100

    Initial commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Antoine SALLIOU on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
